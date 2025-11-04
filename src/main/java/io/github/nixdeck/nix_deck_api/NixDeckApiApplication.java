8626129eba83fab16b70b8ac2a852534efd813dd
Duppps <ruanvasconcelos898@gmail.com>
2025-11-03 23:06:33 -0300

    initial commit

---
+++ src/main/java/io/github/nixdeck/nix_deck_api/NixDeckApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.nixdeck.nix_deck_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NixDeckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NixDeckApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nixdeck/nix_deck_api/NixDeckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nixdeck.nix_deck_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NixDeckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
